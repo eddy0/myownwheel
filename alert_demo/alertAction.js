e2edb8310960e15b8c502cda10ce1550c475dd8d
eddy0 <awindy369@gmail.com>
2018-04-14 21:13:34 -0700

    a small issue on line

--- alert_demo/alertAction.js
+++ alert_demo/alertAction.js
@@ -35,168 +35,167 @@ class Alert {
 
     cssTemplate() {
         const t = `
-      <style id="id-style-alert" >
-        .wd-alert-container, .wd-alert-container * {
-            margin: 0;
-            padding: 0;
-            font: inherit;
-            color: inherit;
-            box-sizing: border-box;
-        }
-        .wd-alert-container{
-            font-size: 14px;
-            font-family: Helvetica,Arial,sans-serif;
-        }
-        .wd-alert-container a{
-            text-decoration: none;
-        }
-        .wd-alert-container img{
-            vertical-align: top;
-        }
-        .wd-alert-container.alert-show div{
-            display: block;
-            opacity: 1
-        }
+          <style id="id-style-alert" >
+            .wd-alert-container, .wd-alert-container * {
+                margin: 0;
+                padding: 0;
+                font: inherit;
+                color: inherit;
+                box-sizing: border-box;
+            }
+            .wd-alert-container{
+                font-size: 14px;
+                font-family: Helvetica,Arial,sans-serif;
+            }
+            .wd-alert-container a{
+                text-decoration: none;
+            }
+            .wd-alert-container img{
+                vertical-align: top;
+            }
+            .wd-alert-container.alert-show div{
+                display: block;
+                opacity: 1
+            }
 
-        .wd-alert-overlay{
-            display: none;
-            opacity: 0;
-            background-color: rgba(0,0,0, 0.4);
-            left: 0;
-            right: 0;
-            top: 0;
-            bottom: 0;
-            position: absolute;
-            z-index: 998;
-        }
-        .wd-alert-box{
-            display: none;
-            width: 300px;
-            background-color: #fff;
-            position: fixed;
-            left: 50%;
-            top: 50%;
-            padding: 20px;
-            border-radius: 5px;
-            text-align: center;
-            transform: translate(-50%, -50%);
-            -webkit-transform: translate(-50%, -50%);
-            animation: wdAlertShow 0.3s;
-            z-index: 999;
-        }
-        @keyframes wdAlertShow {
-            0% {
-                -webkit-transform:translate(-50%, -50%)  scale(.7);
-                transform:translate(-50%, -50%)  scale(.7);
+            .wd-alert-overlay{
+                display: none;
+                opacity: 0;
+                background-color: rgba(0,0,0, 0.4);
+                left: 0;
+                right: 0;
+                top: 0;
+                bottom: 0;
+                position: absolute;
+                z-index: 998;
+            }
+            .wd-alert-box{
+                display: none;
+                width: 300px;
+                background-color: #fff;
+                position: fixed;
+                left: 50%;
+                top: 50%;
+                padding: 20px;
+                border-radius: 5px;
+                text-align: center;
+                transform: translate(-50%, -50%);
+                -webkit-transform: translate(-50%, -50%);
+                animation: wdAlertShow 0.3s;
+                z-index: 999;
+            }
+            @keyframes wdAlertShow {
+                0% {
+                    -webkit-transform:translate(-50%, -50%)  scale(.7);
+                    transform:translate(-50%, -50%)  scale(.7);
+                    }
+                45% {
+                    -webkit-transform:translate(-50%, -50%)  scale(1.05);
+                    transform:translate(-50%, -50%)  scale(1.05);
                 }
-            45% {
-                -webkit-transform:translate(-50%, -50%)  scale(1.05);
-                transform:translate(-50%, -50%)  scale(1.05);
+                80% {
+                    -webkit-transform:translate(-50%, -50%)  scale(.95);
+                    transform:translate(-50%, -50%)  scale(.95);
+                }
+                100% {
+                    -webkit-transform:translate(-50%, -50%)  scale(1);
+                    transform:translate(-50%, -50%)  scale(1);
+                }
             }
-            80% {
-                -webkit-transform:translate(-50%, -50%)  scale(.95);
-                transform:translate(-50%, -50%)  scale(.95);
+            .wd-alert-box > .wd-alert-title{
+                color: #575757;
+                font-size: 30px;
+                text-align: center;
+                font-weight: 600;
+                text-transform: none;
+                position: relative;
+                margin: 0;
+                padding: 0;
+                line-height: 60px;
+                display: block;
             }
-            100% {
-                -webkit-transform:translate(-50%, -50%)  scale(1);
-                transform:translate(-50%, -50%)  scale(1);
+            .wd-alert-box  .wd-alert-content {
+                font-size: 18px;
+                text-align: center;
+                font-weight: 300;
+                position: relative;
+                float: none;
+                margin: 0;
+                padding: 0;
+                line-height: normal;
+                color: #555;
             }
-        }
-        .wd-alert-box > .wd-alert-title{
-            color: #575757;
-            font-size: 30px;
-            text-align: center;
-            font-weight: 600;
-            text-transform: none;
-            position: relative;
-            margin: 0;
-            padding: 0;
-            line-height: 60px;
-            display: block;
-        }
-        .wd-alert-box  .wd-alert-content {
-            font-size: 18px;
-            text-align: center;
-            font-weight: 300;
-            position: relative;
-            float: none;
-            margin: 0;
-            padding: 0;
-            line-height: normal;
-            color: #555;
-        }
-        
-        .wd-alert-box  .wd-alert-input{
-            width: 100%;
-            box-sizing: border-box;
-            border-radius: 3px;
-            border: 1px solid #d7d7d7;
-            margin: 10px auto;
-            font-size: 18px;
-            box-shadow: inset 0px 1px 1px rgba(0, 0, 0, 0.3);
-            -webkit-transition: all 0.3s;
-            transition: all 0.3s;
-            height: 40px;
-            padding: 0 12px;
-        
-         }
-         
-        .wd-alert-box .wd-alert-btns{
-            margin: 30px auto 0;
-        }
-        .wd-alert-box .wd-alert-btn{
-            margin: 0 5px;
-            color: #fff;
-            border: 0;
-            box-shadow: none;
-            font-size: 17px;
-            font-weight: 500;
-            border-radius: 5px;
-            padding: 10px 32px;
-            cursor: pointer;
-            box-sizing: border-box;
-            background-color: #999;
-        }
-        
-        .wd-alert-btn:hover{
-            background-color: #888;
-        }
-        
-         .wd-alert-box .wd-alert-btn:focus{
-            outline: 0;
-            border: 0;
-        }
-        
-        .wd-alert-btn.wd-alert-submit, .wd-alert-btn.wd-alert-notice {
-             background-color: rgb(48, 133, 214);
-        }
-        .wd-alert-btn.wd-alert-submit, .wd-alert-btn.wd-alert-notice:hover{
-            background-color: rgb(43, 120, 193);
-        }
-        
-         .wd-alert-btn.wd-alert-confirm {
-             background-color: rgb(92,184,92);
-        }
-        .wd-alert-btn.wd-alert-confirm:hover{
-            background-color: rgb(68,157,68);
-        }
-        
-       .wd-alert-btn.wd-alert-cancel{
-            background-color: #999;
-        }
-        .wd-alert-btn.wd-alert-cancel:hover{
-            background-color: #888;
-        }
-       
-         .wd-alert-btn.wd-alert-reject{
-            background-color: #d9534f;
-        }
-         .wd-alert-btn.wd-alert-reject:hover{
-             background-color: #c9302c;
-         }
 
-    </style>
-    `
+            .wd-alert-box  .wd-alert-input{
+                width: 100%;
+                box-sizing: border-box;
+                border-radius: 3px;
+                border: 1px solid #d7d7d7;
+                margin: 10px auto;
+                font-size: 18px;
+                box-shadow: inset 0px 1px 1px rgba(0, 0, 0, 0.3);
+                -webkit-transition: all 0.3s;
+                transition: all 0.3s;
+                height: 40px;
+                padding: 0 12px;
+
+             }
+
+            .wd-alert-box .wd-alert-btns{
+                margin: 30px auto 0;
+            }
+            .wd-alert-box .wd-alert-btn{
+                margin: 0 5px;
+                color: #fff;
+                border: 0;
+                box-shadow: none;
+                font-size: 17px;
+                font-weight: 500;
+                border-radius: 5px;
+                padding: 10px 32px;
+                cursor: pointer;
+                box-sizing: border-box;
+                background-color: #999;
+            }
+
+            .wd-alert-btn:hover{
+                background-color: #888;
+            }
+
+             .wd-alert-box .wd-alert-btn:focus{
+                outline: 0;
+                border: 0;
+            }
+
+            .wd-alert-btn.wd-alert-submit, .wd-alert-btn.wd-alert-notice {
+                 background-color: rgb(48, 133, 214);
+            }
+            .wd-alert-btn.wd-alert-submit, .wd-alert-btn.wd-alert-notice:hover{
+                background-color: rgb(43, 120, 193);
+            }
+
+             .wd-alert-btn.wd-alert-confirm {
+                 background-color: rgb(92,184,92);
+            }
+            .wd-alert-btn.wd-alert-confirm:hover{
+                background-color: rgb(68,157,68);
+            }
+
+           .wd-alert-btn.wd-alert-cancel{
+                background-color: #999;
+            }
+            .wd-alert-btn.wd-alert-cancel:hover{
+                background-color: #888;
+            }
+
+             .wd-alert-btn.wd-alert-reject{
+                background-color: #d9534f;
+            }
+             .wd-alert-btn.wd-alert-reject:hover{
+                 background-color: #c9302c;
+             }
+        </style>
+        `
         return t
     }
 
